6705132fb6ae8839f8a1790771c5f5409895bda7
yili <yili@yahoo-inc.com>
2014-01-28 01:26:57 -0800

    based on the search favorate activity, build up a SearchFilter Object, then convert this object to json format and save to the sharedPreference storage for persistence. Also, created a Preference class for save and get.

--- Foodie/src/com/yahoo/foodie/activity/SearchFavoriteActivity.java
+++ Foodie/src/com/yahoo/foodie/activity/SearchFavoriteActivity.java
@@ -6,19 +6,47 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.yahoo.foodie.models.SearchFilter;
+import com.yahoo.foodie.persistence.FoodiePreference;
 import com.yahoo.group12.foodie.R;
 
 public class SearchFavoriteActivity extends Activity {
+
+	private EditText etSearchTerm;
+	private EditText etLocation;
+	private Spinner spDistance;
+	private Spinner spSortBy;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_favorite);
+		
+		setupViews();
+		
+		SearchFilter filter = FoodiePreference.get(this);
+		if (filter != null) {
+			Log.d("DEBUG", "sort by: " + filter.getSortBy());
+			Log.d("DEBUG", "search term: " + filter.getQueryTerm());
+			Log.d("DEBUG", "location: " + filter.getLocation());
+		} else {
+			Log.d("DEBUG", "filter is null");
+		}
+	}
+
+	private void setupViews() {
+		etSearchTerm = (EditText)findViewById(R.id.etQuery);
+		etLocation = (EditText) findViewById(R.id.etLocation);
+		spDistance = (Spinner) findViewById(R.id.spDistance);
+		spSortBy = (Spinner) findViewById(R.id.spSortBy);	
+		
 	}
 
 	@Override
@@ -44,7 +72,28 @@ public class SearchFavoriteActivity extends Activity {
 	}
 	
 	public void onSubmitSearch(MenuItem mi) {
-		return;
+		
+		String queryTerm = etSearchTerm.getText().toString();
+		String location = etLocation.getText().toString();
+		String sortBy = spSortBy.getSelectedItem().toString();
+		String distanceStr = spDistance.getSelectedItem().toString();
+		float distance; // this may need to be double for precision
+		if ("Auto".equals(distanceStr)) {
+			distance = 0.5f; //? what does auto means here?
+		}
+		try {
+			distance = Float.parseFloat(distanceStr);
+		} catch (NumberFormatException ne) {
+			distance = 0.5f;
+		}
+		
+		
+		SearchFilter filterPref = new SearchFilter.Builder(queryTerm)
+											   .setLocation(location)
+								               .setSortBy(sortBy)
+								               .setDistance(distance)
+								               .build();
+		new FoodiePreference(this, filterPref).save();
 	}
 
 }
