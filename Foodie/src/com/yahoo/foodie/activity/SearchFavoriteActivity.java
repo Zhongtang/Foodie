72af780a5522583e6ce714b54206c07c1c3fb568
yili <yili@yahoo-inc.com>
2014-02-13 01:42:44 -0800

    move control/logics from search favorite activity to search favorite fragment such that the fragment is self-contained. the activity calls method on the fragment to save preference.

--- Foodie/src/com/yahoo/foodie/activity/SearchFavoriteActivity.java
+++ Foodie/src/com/yahoo/foodie/activity/SearchFavoriteActivity.java
@@ -1,87 +1,26 @@
 package com.yahoo.foodie.activity;
 
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.ActionBar;
-import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.yahoo.foodie.app.YelpClient;
-import com.yahoo.foodie.app.YelpClientApp;
-import com.yahoo.foodie.models.Business;
-import com.yahoo.foodie.models.SearchFilter;
-import com.yahoo.foodie.persistence.FoodiePreference;
+import com.yahoo.foodie.fragments.SearchFavoriteFragment;
 import com.yahoo.group12.foodie.R;
 
 public class SearchFavoriteActivity extends FragmentActivity {
-
-	private EditText etSearchTerm;
-	private EditText etLocation;
-	private Spinner spDistance;
-	private Spinner spSortBy;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_favorite);
-		
-		setupViews();
-		
-		SearchFilter filter = FoodiePreference.get(this);
-		if (filter != null) {
-			Log.d("DEBUG", "sort by: " + filter.getSortBy());
-			Log.d("DEBUG", "search term: " + filter.getQueryTerm());
-			Log.d("DEBUG", "location: " + filter.getLocation());
-		} else {
-			Log.d("DEBUG", "filter is null");
-		}
-		
-		YelpClient client = YelpClientApp.getRestClient();
-        client.search("food", "san francisco", new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int code, JSONObject body) {
-                Log.d("DEBUG", "onSuccess");
-                try {
-                    JSONArray businessesJson = body.getJSONArray("businesses");
-                    ArrayList<Business> businesses = Business.fromJson(businessesJson);
-                    Log.d("DEBUG", businesses.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            
-            @Override
-            public void onFailure(Throwable arg0) {
-                Toast.makeText(SearchFavoriteActivity.this, "FAIL", Toast.LENGTH_LONG).show();
-            }
-        });
-	    
 	}
 	
-	private void setupViews() {
-		etSearchTerm = (EditText)findViewById(R.id.etQuery);
-		etLocation = (EditText) findViewById(R.id.etLocation);
-		spDistance = (Spinner) findViewById(R.id.spDistance);
-		spSortBy = (Spinner) findViewById(R.id.spSortBy);
-		
-	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		initActionBar();
@@ -105,28 +44,8 @@ public class SearchFavoriteActivity extends FragmentActivity {
 	}
 	
 	public void onSubmitSearch(MenuItem mi) {
-		
-		String queryTerm = etSearchTerm.getText().toString();
-		String location = etLocation.getText().toString();
-		String sortBy = spSortBy.getSelectedItem().toString();
-		String distanceStr = spDistance.getSelectedItem().toString();
-		float distance; // this may need to be double for precision
-		if ("Auto".equals(distanceStr)) {
-			distance = 0.5f; //? what does auto means here?
-		}
-		try {
-			distance = Float.parseFloat(distanceStr);
-		} catch (NumberFormatException ne) {
-			distance = 0.5f;
-		}
-		
-		
-		SearchFilter filterPref = new SearchFilter.Builder(queryTerm)
-											   .setLocation(location)
-								               .setSortBy(sortBy)
-								               .setDistance(distance)
-								               .build();
-		new FoodiePreference(this, filterPref).save();
+		SearchFavoriteFragment prefs = (SearchFavoriteFragment) getSupportFragmentManager().findFragmentById(R.id.fgmtSearchOptions);
+		prefs.onSavePreference();
 	}
 
 }
--- Foodie/src/com/yahoo/foodie/fragments/SearchFavoriteFragment.java
+++ Foodie/src/com/yahoo/foodie/fragments/SearchFavoriteFragment.java
@@ -1,18 +1,80 @@
 package com.yahoo.foodie.fragments;
 
-
-import com.yahoo.group12.foodie.R;
-
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.yahoo.foodie.models.SearchFilter;
+import com.yahoo.foodie.persistence.FoodiePreference;
+import com.yahoo.group12.foodie.R;
 
 public class SearchFavoriteFragment extends Fragment {
+    FragmentActivity listener;
+    private EditText etSearchTerm;
+    private EditText etLocation;
+    private Spinner spDistance;
+    private Spinner spSortBy;
+    
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_search_favorite, container, false);
+		View v = inflater.inflate(R.layout.fragment_search_favorite, container, false);
+		setupViews(v);
+		return v;
 	}
+	
+	@Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.listener = (FragmentActivity) activity;
+    }
+
+    private void setupViews(View v) {
+        etSearchTerm = (EditText)v.findViewById(R.id.etQuery);
+        etLocation = (EditText)v.findViewById(R.id.etLocation);
+        spDistance = (Spinner)v.findViewById(R.id.spDistance);
+        spSortBy = (Spinner)v.findViewById(R.id.spSortBy);
+        
+        SearchFilter filter = FoodiePreference.get(getActivity().getApplicationContext());
+        if (filter != null) {
+            Log.d("DEBUG", "sort by: " + filter.getSortBy());
+            Log.d("DEBUG", "search term: " + filter.getQueryTerm());
+            Log.d("DEBUG", "location: " + filter.getLocation());
+        } else {
+            Log.d("DEBUG", "filter is null");
+        }
+        
+    }
+	
+	public void onSavePreference() {
+        
+        String queryTerm = etSearchTerm.getText().toString();
+        String location = etLocation.getText().toString();
+        String sortBy = spSortBy.getSelectedItem().toString();
+        String distanceStr = spDistance.getSelectedItem().toString();
+        float distance; // this may need to be double for precision
+        if ("Auto".equals(distanceStr)) {
+            distance = 0.5f; //? what does auto means here?
+        }
+        try {
+            distance = Float.parseFloat(distanceStr);
+        } catch (NumberFormatException ne) {
+            distance = 0.5f;
+        }       
+        
+        SearchFilter filterPref = new SearchFilter.Builder(queryTerm)
+                                               .setLocation(location)
+                                               .setSortBy(sortBy)
+                                               .setDistance(distance)
+                                               .build();
+        
+        new FoodiePreference(getActivity().getApplicationContext(), filterPref).save();
+    }
 
 }
