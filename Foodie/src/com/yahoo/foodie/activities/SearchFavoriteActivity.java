d0e3b0363536d39b793e72ee8ab54c5493edfc72
yili <yili@yahoo-inc.com>
2014-02-24 00:21:07 -0800

    move the new friend methods from SearchFavoriteActivity to RestaurantListActivity as the former is deprecated now.

--- Foodie/src/com/yahoo/foodie/activities/SearchFavoriteActivity.java
+++ Foodie/src/com/yahoo/foodie/activities/SearchFavoriteActivity.java
@@ -38,7 +38,8 @@ public class SearchFavoriteActivity extends RootActivity {
 		// Zenith: Parse.initialize(this, "Iu7WYT2uS0CGn6HP1X6Rj2QscU1KyirbAyGyGhPd", "vNum3crFgtTs2dLSJPwydKXpzYqftbpZI44APfKa");
 		// Zhongtang: Parse.initialize(this, "8XfMtfEQT44xBf8qS8iucwai3JTfZOmg2EF4Yr4w", "uANkznUoVmJVgzn484HYWHTOrC97bxIR8aPQ1CeR");
 		ParseObject.registerSubclass(Friends.class);	// register need to be called before initialize
-		Parse.initialize(this, "8XfMtfEQT44xBf8qS8iucwai3JTfZOmg2EF4Yr4w", "uANkznUoVmJVgzn484HYWHTOrC97bxIR8aPQ1CeR");
+		//Parse.initialize(this, "8XfMtfEQT44xBf8qS8iucwai3JTfZOmg2EF4Yr4w", "uANkznUoVmJVgzn484HYWHTOrC97bxIR8aPQ1CeR");
+		Parse.initialize(this, "1RPMooDcVnHYlBowXpHdCwU1qzTCyDc46XuY80AK", "Bx6tXHCtOnVGZiDiKmHoueiIaTgQSgtdzUlIy8YK");
 
 
 		// For test:
@@ -57,7 +58,7 @@ public class SearchFavoriteActivity extends RootActivity {
 		//makeFriends("stone", "zenith");
 		
 		// user session log in
-		userLogIn("stone", "iamstone");
+		//userLogIn("stone", "iamstone");
 	}
 
 	@Override
@@ -67,98 +68,98 @@ public class SearchFavoriteActivity extends RootActivity {
 		return true;
 	}
 	
-	private void userSignUp(String username, String password) {
-		// TODO: check syntax
-		// TODO: add phone# so that it's possible to sync with local contact list
-		ParseUser user = new ParseUser();
-		user.setUsername(username);
-		user.setPassword(password);
-		user.signUpInBackground(new SignUpCallback() {
-			public void done(ParseException e) {
-				if (e == null) {
-					Toast.makeText(self, "Successfully Signed Up!", Toast.LENGTH_SHORT).show();
-				} else {
-					Toast.makeText(self, "Failed to Sign Up!", Toast.LENGTH_SHORT).show();
-				}
-			}
-		});
-	}
-	
-	private void userLogIn(final String username, String password) {
-		ParseUser.logInInBackground(username, password, new LogInCallback() {
-			public void done(ParseUser user, ParseException e) {
-				if (e == null) {
-					Toast.makeText(self, "YOU ARE " + user.getUsername(), Toast.LENGTH_SHORT).show();
-					// Calling pushSetup, getMyFriends and pushToUsers here to test their functionality
-					pushSetup(ParseUser.getCurrentUser().getUsername());
-					getMyFriends(ParseUser.getCurrentUser().getUsername());
-					ArrayList<String> targets = new ArrayList<String>();
-					targets.add(ParseUser.getCurrentUser().getUsername());
-					pushToUsers(targets, "LET'S HAVE LUNCH");
-				} else {
-					Toast.makeText(self, "Failed to Log in!", Toast.LENGTH_SHORT).show();
-				}
-			}
-		});
-	}
-	
-	private void makeFriends(String userReq, String userAcp) {
-		Friends friends = new Friends();
-		friends.makeFriends(userReq, userAcp);
-		friends.saveInBackground();
-	}
-	
-	// Given a string username, fetch all his friends username
-	public void getMyFriends(final String myUsername) {
-		ParseQuery<Friends> queryUserReq = ParseQuery.getQuery(Friends.class);
-		queryUserReq.whereEqualTo("userReq", myUsername);
-		
-		ParseQuery<Friends> queryUserAcp = ParseQuery.getQuery(Friends.class);
-		queryUserAcp.whereEqualTo("userAcp", myUsername);
-		
-		List<ParseQuery<Friends>> queries = new ArrayList<ParseQuery<Friends>>();
-		queries.add(queryUserReq);
-		queries.add(queryUserAcp);
-		
-		ParseQuery<Friends> mainQuery = ParseQuery.or(queries);
-		mainQuery.findInBackground(new FindCallback<Friends>() {
-		  public void done(List<Friends> results, ParseException e) {
-		        if (e == null) {
-		            // Access the array of results here
-		        	String allMyFriends = "";
-		        	for(int i = 0; i < results.size(); i++) {
-		        		allMyFriends += (" " + results.get(i).getFriendAfterQuery(myUsername));
-		        	}
-		            Toast.makeText(self, "Your Friends: " + allMyFriends, Toast.LENGTH_SHORT).show();
-		        } else {
-		            Log.d("item", "Error: " + e.getMessage());
-		        }
-		  }
-		});
-	}
-	
-	// Setup push for a given user
-	private void pushSetup(String username) {
-		ParseInstallation installation = ParseInstallation.getCurrentInstallation();
-		installation.put("username", username);
-		installation.saveInBackground();
-	}
-	
-	
-	// Given an array of usernames, send push notification to all these users
-	public void pushToUsers(ArrayList<String> targets, String message) {
-		List<ParseQuery<ParseInstallation>> queries = new ArrayList<ParseQuery<ParseInstallation>>();
-		for(int i = 0; i < targets.size(); i++) {
-			ParseQuery<ParseInstallation> query = ParseInstallation.getQuery();;
-			query.whereEqualTo("username", targets.get(i));
-			queries.add(query);
-    	}
-		
-		ParseQuery<ParseInstallation> mainQuery = ParseQuery.or(queries);
-		ParsePush push = new ParsePush();
-		push.setQuery(mainQuery);
-		push.setMessage(message);
-		push.sendInBackground();
-
-	}
+//	private void userSignUp(String username, String password) {
+//		// TODO: check syntax
+//		// TODO: add phone# so that it's possible to sync with local contact list
+//		ParseUser user = new ParseUser();
+//		user.setUsername(username);
+//		user.setPassword(password);
+//		user.signUpInBackground(new SignUpCallback() {
+//			public void done(ParseException e) {
+//				if (e == null) {
+//					Toast.makeText(self, "Successfully Signed Up!", Toast.LENGTH_SHORT).show();
+//				} else {
+//					Toast.makeText(self, "Failed to Sign Up!", Toast.LENGTH_SHORT).show();
+//				}
+//			}
+//		});
+//	}
+//	
+//	private void userLogIn(final String username, String password) {
+//		ParseUser.logInInBackground(username, password, new LogInCallback() {
+//			public void done(ParseUser user, ParseException e) {
+//				if (e == null) {
+//					Toast.makeText(self, "YOU ARE " + user.getUsername(), Toast.LENGTH_SHORT).show();
+//					// Calling pushSetup, getMyFriends and pushToUsers here to test their functionality
+//					pushSetup(ParseUser.getCurrentUser().getUsername());
+//					getMyFriends(ParseUser.getCurrentUser().getUsername());
+//					ArrayList<String> targets = new ArrayList<String>();
+//					targets.add(ParseUser.getCurrentUser().getUsername());
+//					pushToUsers(targets, "LET'S HAVE LUNCH");
+//				} else {
+//					Toast.makeText(self, "Failed to Log in!", Toast.LENGTH_SHORT).show();
+//				}
+//			}
+//		});
+//	}
+//	
+//	private void makeFriends(String userReq, String userAcp) {
+//		Friends friends = new Friends();
+//		friends.makeFriends(userReq, userAcp);
+//		friends.saveInBackground();
+//	}
+//	
+//	// Given a string username, fetch all his friends username
+//	public void getMyFriends(final String myUsername) {
+//		ParseQuery<Friends> queryUserReq = ParseQuery.getQuery(Friends.class);
+//		queryUserReq.whereEqualTo("userReq", myUsername);
+//		
+//		ParseQuery<Friends> queryUserAcp = ParseQuery.getQuery(Friends.class);
+//		queryUserAcp.whereEqualTo("userAcp", myUsername);
+//		
+//		List<ParseQuery<Friends>> queries = new ArrayList<ParseQuery<Friends>>();
+//		queries.add(queryUserReq);
+//		queries.add(queryUserAcp);
+//		
+//		ParseQuery<Friends> mainQuery = ParseQuery.or(queries);
+//		mainQuery.findInBackground(new FindCallback<Friends>() {
+//		  public void done(List<Friends> results, ParseException e) {
+//		        if (e == null) {
+//		            // Access the array of results here
+//		        	String allMyFriends = "";
+//		        	for(int i = 0; i < results.size(); i++) {
+//		        		allMyFriends += (" " + results.get(i).getFriendAfterQuery(myUsername));
+//		        	}
+//		            Toast.makeText(self, "Your Friends: " + allMyFriends, Toast.LENGTH_SHORT).show();
+//		        } else {
+//		            Log.d("item", "Error: " + e.getMessage());
+//		        }
+//		  }
+//		});
+//	}
+//	
+//	// Setup push for a given user
+//	private void pushSetup(String username) {
+//		ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+//		installation.put("username", username);
+//		installation.saveInBackground();
+//	}
+//	
+//	
+//	// Given an array of usernames, send push notification to all these users
+//	public void pushToUsers(ArrayList<String> targets, String message) {
+//		List<ParseQuery<ParseInstallation>> queries = new ArrayList<ParseQuery<ParseInstallation>>();
+//		for(int i = 0; i < targets.size(); i++) {
+//			ParseQuery<ParseInstallation> query = ParseInstallation.getQuery();;
+//			query.whereEqualTo("username", targets.get(i));
+//			queries.add(query);
+//    	}
+//		
+//		ParseQuery<ParseInstallation> mainQuery = ParseQuery.or(queries);
+//		ParsePush push = new ParsePush();
+//		push.setQuery(mainQuery);
+//		push.setMessage(message);
+//		push.sendInBackground();
+//
+//	}
 }
